1db0b780171468f04dcc14dfffe4dfc92c8aa64d
Wilson <wilsonlimmmm@gmail.com>
2023-07-08 01:26:33 +0700

    use useEffect to handle isSucess

--- client/src/hooks/useCreatePost.ts
+++ client/src/hooks/useCreatePost.ts
@@ -1,6 +1,7 @@
 import { useCreatePostMutation } from "@/redux/api/postApi";
 import { ICreatePostDto } from "@/redux/interfaces";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export const useCreatePost = () => {
 	const router = useRouter();
@@ -8,10 +9,12 @@ export const useCreatePost = () => {
 		useCreatePostMutation();
 
 	const createPost = async (data: ICreatePostDto) => {
-		const result = await createPostMutation(data);
-
-		if (isSuccess) router.push("/home");
+		await createPostMutation(data);
 	};
+
+	useEffect(() => {
+		if (isSuccess) router.push("/home");
+	}, [isSuccess]);
 
 	return { createPost, isLoading };
 };
